09aab67e8f91aa92bd18aef00e068f6e767b0fb2
Craig <craig@blokeskate.dev>
2026-06-06 14:17:10 +0100

    fixup! feat: Add traefik container for https redirection

--- dcp/settings.py
+++ dcp/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "True")
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definitionc
 
