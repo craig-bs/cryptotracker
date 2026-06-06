2d00d20888f901af0ba5a231037700e13b1e84c1
Craig <craig@blokeskate.dev>
2026-06-06 11:41:40 +0100

    fixup! fixup! fixup! feat: Add traefik container for https redirection

--- dcp/settings.py
+++ dcp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "True")
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOW_HOSTS", "127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 
 # Application definitionc
